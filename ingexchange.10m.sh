63a4798a15cc7716c15722b7a081de821627151e
us3r64 <us3r64@gmail.com>
2016-05-26 10:49:15 +0300

    Update: metadata title, github, desc

--- ingexchange.10m.sh
+++ ingexchange.10m.sh
@@ -7,12 +7,12 @@
 # Shows current track information from http://ingexchange-nibbler.rhcloud.com
 
 # metadata
-# <bitbar.title>ING exchange</bitbar.title>
+# <bitbar.title>exchange</bitbar.title>
 # <bitbar.version>v1.0</bitbar.version>
 # <bitbar.author>us3r64</bitbar.author>
-# <bitbar.author.github>us3r64</bitbar.author.github>
-# <bitbar.desc>Shows current track information from http://ingexchange-nibbler.rhcloud.com</bitbar.desc>
-# <bitbar.image>-</bitbar.image>
+# <bitbar.author.github>https://github.com/us3r64/exchange</bitbar.author.github>
+# <bitbar.desc>Shows current track information for USD from http://ingexchange-nibbler.rhcloud.com for ING/TR(transilvania) banks</bitbar.desc>
+# <bitbar.image></bitbar.image>
 
 curl --get --include "http://ingexchange-nibbler.rhcloud.com" -s | grep "ING: C.*USD:*" | awk -F ":" '{gsub(/^[ \t]+|[ \t]+$/,"",$4);print "USD:"$4"| color=green"}'
 

f8c8254fee206883e775b43c977c68a0c125c017
Dorin Strimbu <us3r64@gmail.com>
2019-01-08 00:21:41 +0200

    Update new domain ex.rm-rf.ro

--- ingexchange.10m.sh
+++ ingexchange.10m.sh
@@ -1,22 +1,39 @@
 #!/bin/bash
 
-# Get current ING exchange
+# Get exchange rates that i'm intrested in
 #
 # by us3r64@gmail.com
 #
-# Shows current track information from http://ingexchange-nibbler.rhcloud.com
+# Shows current exchange rates information from http://ex.rm-rf.ro
 
 # metadata
 # <bitbar.title>exchange</bitbar.title>
 # <bitbar.version>v1.0</bitbar.version>
 # <bitbar.author>us3r64</bitbar.author>
-# <bitbar.author.github>https://github.com/us3r64/exchange</bitbar.author.github>
-# <bitbar.desc>Shows current track information for USD from http://ingexchange-nibbler.rhcloud.com for ING/TR(transilvania) banks</bitbar.desc>
-# <bitbar.image></bitbar.image>
+# <bitbar.author.github>us3r64</bitbar.author.github>
+# <bitbar.desc>Shows current track information from http://ex.rm-rf.ro</bitbar.desc>
+# <bitbar.image>-</bitbar.image>
 
-curl --get --include "http://ingexchange-nibbler.rhcloud.com" -s | grep "ING: C.*USD:*" | awk -F ":" '{gsub(/^[ \t]+|[ \t]+$/,"",$4);print "USD:"$4"| color=green"}'
+((COUNT = 4))
+while [[ $COUNT -ne 0 ]] ; do
+    ping -q -c 1 8.8.8.8 &> /dev/null
+    RC=$?
+    if [[ $RC -eq 0 ]] ; then
+        ((COUNT = 1))
+    fi
+    ((COUNT = COUNT - 1))
+done
+if [[ $RC -eq 0 ]] ; then
+    STATUS="online"
 
-echo "---"
-curl --get --include "http://ingexchange-nibbler.rhcloud.com" -s | grep "USD*"
+    curl --get --include "http://ex.rm-rf.ro" -s | grep "ING: C.*EUR :*" | awk -F ":" '{gsub(/^[ \t]+|[ \t]+$/,"",$4);print $4"| color=green"}'
+    # curl --get --include "http://ex.rm-rf.ro" -s | grep "BTC:" | awk -F ":" '{gsub(/^[ \t]+|[ \t]+$/,"",$4);print int($4)"| color=green"}'
 
-echo "Refresh | refresh=true"
+    echo "---"
+    curl --get --include "http://ex.rm-rf.ro" -s | grep "EUR*\|1 euro"
+
+    echo "Refresh | refresh=true"
+
+else
+    STATUS="offline"
+fi
